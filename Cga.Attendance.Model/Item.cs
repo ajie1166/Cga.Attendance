81b290a3c636e7d4e0bce973ff7def2a795c480c
李杰 <joven.lee@hotmail.com>
2017-05-23 16:39:22 +0800

    创建CGA项目

---
+++ Cga.Attendance.Model/Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cga.Attendance.Model
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string ItemName { get; set; }
+
+        public int? ParentId { get; set; }
+
+        [StringLength(255)]
+        public string ItemUrl { get; set; }
+
+        [StringLength(255)]
+        public string IconUrl { get; set; }
+
+        public DateTime? CreateTime { get; set; }
+    }
+}
---
+++ Cga.Attendance.Model/UserItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cga.Attendance.Model
+{
+    public class UserItem
+    {
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string JobNum { get; set; }
+
+        public int? ItemId { get; set; }
+    }
+}
