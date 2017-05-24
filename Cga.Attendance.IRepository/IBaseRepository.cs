cd04dbd1ec89df4b5ff3112da7ed0d697394010e
李杰 <joven.lee@hotmail.com>
2017-05-24 17:38:27 +0800

    repository

---
+++ Cga.Attendance.IRepository/IBaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cga.Attendance.IRepository
+{
+    public interface IBaseRepository<T> where T : class, new()
+    {
+        int AddEntity(T entity);
+        int DeleteEntity(T entity);
+        int DeleteEntity(Expression<Func<T, bool>> whereLambda);
+        int UpdateEntity(T entity);
+
+
+    }
+}
